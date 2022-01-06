3ceb13606e0c80d33cbbaf1c29adfc9b3895932c
Ayat-Salem <91917437+Ayat-Salem@users.noreply.github.com>
2022-01-06 10:21:46 +0300

    Goal-1
    A Fund creator contract which is the main contract

---
+++ Goal-1.sol
@@ -0,0 +1,7 @@
+pragma solidity ^ 0.8.0;
+
+contract fundCreator {
+
+// This area will be filled with the crowfunding contract and other functions and modifiers as a final step of the project.
+
+}
